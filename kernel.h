8178888636f0b2f06329db969efdaba466269842
Stefano Leto <sleto1999@icloud.com>
2026-02-11 14:22:47 +0100

    Memory management and virtual addressing

--- kernel.h
+++ kernel.h
@@ -9,10 +9,16 @@
         }                                                                     \
     } while (0)
 
-#define PROCS_MAX 8 // Maximum number of processes
-
+#define PROCS_MAX 8     // Maximum number of processes
 #define PROC_UNUSED 0   // Unused process control structure
 #define PROC_RUNNABLE 1 // Runnable process
+
+#define SATP_SV32 (1u << 31)
+#define PAGE_V (1 << 0) // "Valid" bit (entry is enabled)
+#define PAGE_R (1 << 1) // Readable
+#define PAGE_W (1 << 2) // Writable
+#define PAGE_X (1 << 3) // Executable
+#define PAGE_U (1 << 4) // User (accessible in user mode)
 
 struct sbiret
 {
@@ -71,8 +77,9 @@ struct trap_frame
 
 struct process
 {
-    int pid;             // Process ID
-    int state;           // Process state: PROC_UNUSED or PROC_RUNNABLE
-    vaddr_t sp;          // Stack pointer
-    uint8_t stack[8192]; // Kernel stack
+    int pid;              // Process ID
+    int state;            // Process state: PROC_UNUSED or PROC_RUNNABLE
+    vaddr_t sp;           // Stack pointer
+    uint32_t *page_table; // Pointer to the first-level page table
+    uint8_t stack[8192];  // Kernel stack
 };
